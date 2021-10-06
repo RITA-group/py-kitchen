1f1d30c24512e42032cc85f749fb7686582d0eac
Denis <fsdvn1@gmail.com>
2021-10-06 21:05:39 +0300

    Pydantic model version was added.

--- cuisson/main.py
+++ cuisson/main.py
@@ -1,9 +1,20 @@
 from typing import Optional
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 import model
+from pydantic import BaseModel
+
 logger = logging.getLogger(__name__)
 app = FastAPI()
+
+
+class PaginationContainer(BaseModel):
+    """
+    TODO: figure out pagination:
+    https://firebase.google.com/docs/firestore/query-data/query-cursors
+    """
+    result: list
+    cursor: str = 'not-implemented'
 
 
 @app.get("/")
@@ -11,7 +22,35 @@ def read_root():
     return {"py-kitchen": "test test"}
 
 
-@app.get("/users/")
-def list_user():
-    logger.info(model.get_users())
-    return {}
+@app.get("/rooms/")
+def list_rooms() -> PaginationContainer:
+    query = model.Room.collection().stream()
+    container = PaginationContainer(
+        result=[model.Room.from_snapshot(doc) for doc in query]
+    )
+    return container
+
+
+@app.get("/rooms/{room_id}")
+def get_room(room_id: str) -> model.Room:
+    try:
+        room = model.Room.from_id(room_id)
+    except model.NotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Room with {room_id} id doesn't exist."
+        )
+    return room
+
+
+@app.delete("/rooms/{room_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_room(room_id: str):
+    try:
+        room = model.Room.from_id(room_id)
+    except model.NotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Room with {room_id} id doesn't exist."
+        )
+    room.delete()
+    return
--- cuisson/model.py
+++ cuisson/model.py
@@ -1,9 +1,96 @@
 from google.cloud import firestore
+from pydantic import BaseModel
+from pprint import pprint
+
+db = firestore.Client()
+
+"""
+firestore.DocumentSnapshot
+[
+'_client', 
+'_data', '_exists', '_reference', '_to_protobuf', 
+'create_time', 'exists', 'get', 'id', 'read_time', 'reference', 
+'to_dict', 'update_time']
+
+"""
 
 
-def get_users() -> dict:
-    db = firestore.Client()
-    users_ref = db.collection(u'user')
-    docs = users_ref.stream()
-    users = {doc.id: doc.to_dict() for doc in docs}
-    return users
+class NotFound(Exception):
+    pass
+
+
+class FirebaseMixin:
+    @staticmethod
+    def collection():
+        """
+        Specific Firestore root collection
+        """
+        raise NotImplemented
+
+    @staticmethod
+    def convert_fields(data: dict) -> dict:
+        """
+        Convert Firestore formats to python
+        """
+        return {}
+
+    @classmethod
+    def from_snapshot(cls, doc: firestore.DocumentSnapshot):
+        data = doc.to_dict()
+        data['owner'] = data['owner'].path
+        data['id'] = str(doc.id)
+        data.update(cls.convert_fields(data))
+        return cls(**data)
+
+    def delete(self):
+        self.collection().document(self.id).delete()
+
+    @classmethod
+    def from_id(cls, obj_id: str):
+        doc = cls.collection().document(obj_id).get()
+        if not doc.exists:
+            raise NotFound()
+        return cls.from_snapshot(doc)
+
+
+class Room(BaseModel):
+    id: str
+    name: str
+    owner: str
+    token: str
+
+    @staticmethod
+    def collection():
+        return db.collection(u'room')
+
+    @staticmethod
+    def convert_fields(data: dict) -> dict:
+        return {
+            'owner': data['owner'].path
+        }
+
+    @classmethod
+    def from_snapshot(cls, doc: firestore.DocumentSnapshot):
+        data = doc.to_dict()
+        data['owner'] = data['owner'].path
+        data['id'] = str(doc.id)
+        return cls(**data)
+
+    def delete(self):
+        self.collection().document(self.id).delete()
+
+    @classmethod
+    def from_id(cls, obj_id: str):
+        doc = cls.collection().document(obj_id).get()
+        if not doc.exists:
+            raise NotFound()
+        return cls.from_snapshot(doc)
+
+
+class User(BaseModel):
+    id: str
+    name: str
+    answer_count: int
+    hand_up: bool
+    is_answering: bool
+    room: str
