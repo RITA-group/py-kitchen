7bbc87a4c7439a79b256b6e27e81bd50bc08c295
Denis <fsdvn1@gmail.com>
2021-10-20 11:05:46 +0300

    Added room tests.

--- tests/src/test_rooms.py
+++ tests/src/test_rooms.py
@@ -6,16 +6,17 @@ from datetime import datetime
 
 @pytest.fixture
 def rooms(firestore):
-    firestore.collection('rooms').add({
+    _, alpha_ref = firestore.collection('rooms').add({
         'name': 'test room 1',
-        'owner_id': 'some-test-id',
+        'owner_id': 'testtesttest',
         'created': datetime(2021, 1, 1),
     })
-    firestore.collection('rooms').add({
+    _, bravo_ref = firestore.collection('rooms').add({
         'name': 'test room 2',
         'owner_id': 'some-test-id',
         'created': datetime(2021, 1, 2),
     })
+    return alpha_ref, bravo_ref
 
 
 def test_list_rooms(client, rooms):
@@ -27,7 +28,7 @@ def test_list_rooms(client, rooms):
             {
                 'id': ANY,
                 'name': 'test room 1',
-                'owner_id': 'some-test-id',
+                'owner_id': 'testtesttest',
                 'created': '2021-01-01T00:00:00',
             },
             {
@@ -53,3 +54,24 @@ def test_create_room(client):
         'owner_id': 'testtesttest',
         'created': '2021-01-01T00:00:00',
     }
+
+
+def test_delete_room(client, rooms, firestore):
+    response = client.delete(f"/api/v1/rooms/{rooms[0].id}")
+    assert response.status_code == 204
+
+    rooms_left = list(firestore.collection('rooms').stream())
+    assert len(rooms_left) == 1
+    assert rooms_left[0].to_dict() == rooms[1].get().to_dict()
+
+
+def test_delete_other_owner_room_failure(client, rooms, firestore):
+    response = client.delete(f"/api/v1/rooms/{rooms[1].id}")
+    assert response.status_code == 401
+    assert response.json() == {
+        'detail': f"Room {rooms[1].id} doesn't belong to current user."
+    }
+
+    rooms_left = list(firestore.collection('rooms').stream())
+    assert len(rooms_left) == 2
+
